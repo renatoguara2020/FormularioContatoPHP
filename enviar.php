cec8335fc44bc706f4df7e99675d3dd0d5d016bc
Renato Alves Soares <renatoguara2020@gmail.com>
2022-10-29 16:24:22 -0300

    commit changes

---
+++ enviar.php
@@ -0,0 +1,48 @@
+<?php
+date_default_timezone_set('America/Sao_Paulo');
+
+require_once('src/PHPMailer.php');
+require_once('src/SMTP.php');
+require_once('src/Exception.php');
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+if((isset($_POST['email']) && !empty(trim($_POST['email']))) && (isset($_POST['mensagem']) && !empty(trim($_POST['mensagem'])))) {
+
+	$nome = !empty($_POST['nome']) ? $_POST['nome'] : 'Não informado';
+	$email = $_POST['email'];
+	$assunto = !empty($_POST['assunto']) ? utf8_decode($_POST['assunto']) : 'Não informado';
+	$mensagem = $_POST['mensagem'];
+	$data = date('d/m/Y H:i:s');
+
+	$mail = new PHPMailer();
+	$mail->SMTPDebug = 1; // debugging: 1 = errors and messages, 2 = messages only
+    $mail->SMTPAuth = true; // authentication enabled
+    $mail->SMTPSecure = 'ssl'; // secure transfer enabled REQUIRED for Gmail
+	$mail->isSMTP();
+	$mail->Host = 'smtp.gmail.com';
+	// $mail->SMTPAuth = true;
+	$mail->Username = 'renatoguara2020@gmail.com';
+	$mail->Password = 'ag';
+	$mail->Port = 465;
+
+	$mail->setFrom('renatoguara2020@gmail.com');
+	$mail->addAddress('renatoguara2019@yahoo.com');
+
+	$mail->isHTML(true);
+	$mail->Subject = $assunto;
+	$mail->Body = "Nome: {$nome}<br>
+				   Email: {$email}<br>
+				   Mensagem: {$mensagem}<br>
+				   Data/hora: {$data}";
+
+	if($mail->send()) {
+		echo 'Email enviado com sucesso.';
+	} else {
+		echo 'Email não enviado.';
+	}
+} else {
+	echo 'Não enviado: informar o email e a mensagem.';
+}
